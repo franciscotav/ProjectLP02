84289f60c7b0767006ac70b264a5e9f7b21786bb
franciscotav <francisco.tav@ua.pt>
2026-03-19 16:47:37 +0000

    sec

--- GestorKanban/src/main/java/com/mycompany/gestorkanban/GestorKanban.java
+++ GestorKanban/src/main/java/com/mycompany/gestorkanban/GestorKanban.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.gestorkanban;
 
+import interfacegrafica.Janela;
+
 /**
  *
  * @author FranciscoTavares
@@ -11,6 +13,6 @@ package com.mycompany.gestorkanban;
 public class GestorKanban {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Janela janela = new Janela();
     }
 }
---
+++ GestorKanban/src/main/java/interfacegrafica/Janela.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package interfacegrafica;
+import javax.swing.*;
+import java.awt.event.MouseListener;
+ 
+
+/**
+ *
+ * @author FranciscoTavares
+ */
+public class Janela extends JFrame, implements MouseListener {
+    
+    
+    public Janela(){
+         super("Gestor Kaban");
+         super.setSize(1200,780);
+         super.setVisible(true);
+         
+    }
+    
+    
+}
